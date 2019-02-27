16c31d08069211c8a54c79472111608e0efac83d
Rafael Schouten <rafaelschouten@gmail.com>
2019-02-28 03:27:57 +1100

    three locations

--- microclimate/locations.jl
+++ microclimate/locations.jl
@@ -2,10 +2,10 @@ using Revise, Unitful, Microclimate, JLD2, DataStructures
 using Unitful: W, m, °C, hr, mol, K, s, J, Mg, kg, kPa, Pa
 
 basepath = ENV["MICROCLIM"]
-shade = 50
+shade = 0
 years = 2001:2011
 
-skipped = (:snowdepth, :soilwatercontent) 
+skipped = (:soilwatercontent,) 
 envgrid = load_grid(basepath, years, shade, skipped);
 # tas = MicroclimPoint(envgrid, CartesianIndex(56, 53))
 # desert = MicroclimPoint(envgrid, CartesianIndex(30,25))
@@ -13,9 +13,7 @@ envgrid = load_grid(basepath, years, shade, skipped);
 
 t1 = MicroclimPoint(envgrid, CartesianIndex(65, 35))
 t2 = MicroclimPoint(envgrid, CartesianIndex(60, 35))
-t3 = MicroclimPoint(envgrid, CartesianIndex(50, 35))
-t4 = MicroclimPoint(envgrid, CartesianIndex(40, 35))
+t3 = MicroclimPoint(envgrid, CartesianIndex(55, 35))
 
-@save "microclimate/locations.jld" t1 t2 t3 t4
-@save "microclimate/locations.jld" t1 t2 t3 t4
+@save "microclimate/locations.jld" t1 t2 t3
 # @load "microclimate/locations.jld" tas desert qld
