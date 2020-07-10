f3d51019dc56b0e90aaf8a23a9fac7336f1c7dba
Rafael Schouten <rafaelschouten@gmail.com>
2020-07-10 16:17:54 +1000

    add ui description

--- ui.jl
+++ ui.jl
@@ -1,4 +1,21 @@
-# Use a set script pat otherwise the current working directory
+#=
+A DEB model user interface. This allows manual construction
+and parametrisation of a DEB plant model, run with forcing variables
+from microclimate data.
+
+You can swap out any model components by selecting them from the dropdowns
+and hitting the "Reload" button. It should give you an updated set of sliders
+to control the new model with.
+
+Giving the model a name and hitting save will save the load script for the model in the
+models folder. This will be available next time you load the ui.
+If you use an existing name, it will be written over.
+
+If the plot is white initially, just click a flux or vars 
+check box to make it build.
+
+WARNING: this will take a while to load.
+=#
 
 dir = "DEBSCRIPTS" in keys(ENV) ? ENV["DEBSCRIPTS"] : pwd()
 
@@ -15,9 +32,6 @@ models = OrderedDict()
 modeldir = joinpath(dir, "models")
 include.(joinpath.(Ref(modeldir), readdir(modeldir)));
 
-plant = models[:bb];
-@set! plant.environment = environments[:t1];
-
 # Build the app interface
 plotly() # Use Plotly.js. Alternatively use gr()
 plotsize = (2000, 1000) # Set plot size in ui
@@ -25,6 +39,3 @@ app = ModelApp(models, environments, plotsize, tspan, nothing);
 
 # Create an electron desktop app
 electronapp(app; zoom=0.6)
-
-# Save the code for the current state of the model in the app 
-# savecode(app, "maturity")
